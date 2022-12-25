9a0d7f382df2857790b285eb0e2386088d6cb3ba
burak <karanfilkaranfil822@gmail.com>
2022-12-25 19:54:03 +0300

    js

---
+++ assests/js/67-dom-practice.js
@@ -0,0 +1,24 @@
+import { students } from "../data/students.js";
+
+const loadstudents=()=>{
+    let strStudents="";
+    students.forEach((student,index)=>{
+        strStudents+=` <tr>
+        <th scope="row">${index +1}</th>
+        <td>${student.name}</td>
+        <td>${student.puan}</td>
+      </tr>`;
+    });
+    document.querySelector("#tblStudents tbody").innerHTML=strStudents;
+};
+const setStudentBg=()=>{
+    const rows=document.querySelectorAll("#tblStudents tbody tr").innerText;
+    for(let row of rows){
+        const puan=row.querySelector("td:nth-child(3)").innerText;
+    if(puan<50){
+        row.classList.add("table-danger");
+    }
+}
+}
+loadstudents();
+setStudentBg();
